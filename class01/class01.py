6b60c3c81f0cd8d4346e3f9cf76e34f515bfaa65
seokwon.choi <seokwon.choi@giantstepcorp.com>
2023-12-27 11:09:22 +0900

    add class01 selected node

--- class01/class01.py
+++ class01/class01.py
@@ -16,3 +16,13 @@ abcNode.setParms({"ar_filename":filePath})
 
 # 노드간에 연결
 abcNode.setInput(0, camNode)
+
+#####################################################################
+
+import hou
+
+filePath = "/home/seokwon.choi/mySrc"
+
+# 선택한 노드에 경로 넣기
+sel = hou.selectedNodes()
+sel[0].setParms({"shop_propertiespath":filePath})
