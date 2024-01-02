37b2d52a0196d09f6221cc52cd820b2e75d02658
seokwon.choi <seokwon.choi@giantstepcorp.com>
2024-01-02 10:32:12 +0900

    add moveToGoodPosition()

--- class04/class04.py
+++ class04/class04.py
@@ -2,10 +2,12 @@ import hou
 
 def create_volume_material():
     material_node = hou.node("/mat").createNode("arnold_materialbuilder", node_name="volume")
+    material_node.moveToGoodPosition()
     OUT_material_node = material_node.children()[0]
     standard_node = hou.node("/mat/volume").createNode("arnold::standard_volume")
     OUT_material_node.setNamedInput("volume", standard_node, "volume")
-
+    standard_node.moveToGoodPosition()
+    OUT_material_node.moveToGoodPosition()
 
 def select_material_dialog():
     choices = ["Particle", "Surface", "Texture", "Volume"]
@@ -17,20 +19,21 @@ def select_material_dialog():
     
     return selected_option
 
-
 # 노드를 선택한 상태에서 스크립트 시작
 sel = hou.selectedNodes()[0]
 new_geo_node = hou.node("/obj").createNode("geo", node_name=str(sel))
 new_geo_node.setParms({"geo_velocityblur":1})
+new_geo_node.moveToGoodPosition()
 
 # 라이팅 생성
 set_dict = {"back"  : ("rx", 0), "front" : ("rx", 90), "side"  : ("ry", 90)}
 
 for cam_name in list(set_dict.keys()):
     # Litght 노드 생성
-    light_back = hou.node("/obj").createNode("arnold_light", node_name="light_" + cam_name)
-    light_back.setParms({"ar_light_type":1})
-    light_back.setParms({set_dict[cam_name][0]:set_dict[cam_name][1]})
+    light_node = hou.node("/obj").createNode("arnold_light", node_name="light_" + cam_name)
+    light_node.setParms({"ar_light_type":1})
+    light_node.setParms({set_dict[cam_name][0]:set_dict[cam_name][1]})
+    light_node.moveToGoodPosition()
    
 material_result = select_material_dialog()
     
