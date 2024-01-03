1efb1609f8a6030a5e67197a04865930e565aa31
seokwon.choi <seokwon.choi@giantstepcorp.com>
2024-01-03 10:55:27 +0900

    add class05.py

---
+++ class05/class05.py
@@ -0,0 +1,28 @@
+import os
+import hou
+'''
+    파일 패스 다루기
+'''
+cur_file_path = "/projects/2022_09_pipelineEDU2/sequence/EDU/EDU_0020/fx/fx01/dev/scenes/houdini/EDU_0020_fx01_v011.hip"
+
+split_path = cur_file_path.split("/")
+
+project_name = split_path[2]
+shot_name = split_path[5]
+
+pub_abc_path = "/".join(split_path[:-4]) + "pub/abc/"
+
+
+root_path = "/projects/2023_02_theAlien/sequence/BET/BET_0030/camera/pub/abc"
+version_list = sorted(os.listdir(root_path))
+
+version_list.reverse()
+
+os.listdir(root_path + "/" + version_list[0])
+
+
+'''
+    파라미터에 걸려있는 키프레임 삭제
+'''
+sel = hou.selectedNodes()[0]
+sel.parm('f2').deleteAllKeyframes()
