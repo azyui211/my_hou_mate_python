64236a32e15c38704341c6316ec000d248b05af0
seokwon.choi <seokwon.choi@giantstepcorp.com>
2024-01-05 10:48:45 +0900

    add class07

--- class07/class07.py
+++ class07/class07.py
@@ -5,17 +5,24 @@
         QtGui : Font, 마우스 커서, 글자 크기 등 GUI 요소에 대한 부분
         QtUiTools : Designer로 만든 .ui 파일과 관련된 부분
 '''
-
+import os
 import sys
 from PySide2.QtWidgets import QMainWindow, QApplication
-from PySide2.QtUiTools import uic
+from PySide2.QtUiTools import loadUiType
 
-form_class = uic.loadUiType("UI파일이름.ui")[0]
+current_dir_path = os.path.dirname(os.path.abspath(__file__))
+ui_file_path = os.path.join(current_dir_path, "test.ui")        # .ui 파일 연결
+
+form_class = loadUiType(ui_file_path)[0]
 
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.showText)          # 버튼 누르면 showText 함수로 연결
+        
+    def showText(self):
+        self.lineEdit.setText("Test Text!!")                    # LineEdit에 글자 표시
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv) 
