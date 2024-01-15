e3be490ee0ff2698ba660562e10b4d3512e81725
seokwon.choi <seokwon.choi@giantstepcorp.com>
2024-01-15 10:27:16 +0900

    update class11

--- class11/class11.py
+++ class11/class11.py
@@ -137,7 +137,6 @@ for i in file_list:
     dictA[seq_name] = shot_number
 
 
-
 ## 분해
 dictA = {"EDU_0010": "/projects/2022_09_pipelineEDU2/sequence/EDU/EDU_0010",
          "EDU_0020": "/projects/2022_09_pipelineEDU2/sequence/EDU/EDU_0020",
@@ -149,19 +148,25 @@ dictA = {"EDU_0010": "/projects/2022_09_pipelineEDU2/sequence/EDU/EDU_0010",
 dictA_key_list = list(dictA.keys())
 # 마찬가지로 value들로만 묶은 리스트를 만들 수 있습니다.
 dictA_value_list = list(dictA.values())
+
+
+shot_name = "EDU_0030"
+for i in list(dictA.keys()):
+    if shot_name == i:
+        print(dictA[i])
+
 # key, value를 한꺼번에 표시
 for i, k in dictA.items():
-    print(i, k)
-
+    if shot_name == "EDU_0030":
+        print(k)
 
 ## 분해 활용법
 # 상황 예시 : 임의의 샷 이름이 들어왔을 때 딕셔너리 키 값에 매칭되는 샷 이름이면 경로를 반환하는 기능
-shot_name = "EDU_0050"
+shot_name = "EDU_0020"
 dict_key_list = list(dictA.keys())
 if shot_name in dict_key_list:
     shot_path = dictA[shot_name]
     print(shot_path)
-
 
 ## 딕셔너리 안에 딕셔너리
 dictA = {"EDU":{"EDU_0010": "ani01", "EDU_0020": "mmv01"},
@@ -173,11 +178,11 @@ for i in list(dictA.keys()):
     print(i)
 
 
-
 #### 아래의 실습 두 가지는 처음 파이썬을 학습하는 입장에서 난이도가 매우*2 높을 수 있습니다.
 
 ### 문자열 + 리스트 조합 예제
 # 아래의 리스트로 {샷이름:{task이름:파일이름}} 형태의 딕셔너리 만들기
+# ex) {"EDU_0010" : {"ani01":"EDU_0010_ani01_v001_w01.mov"}}
 
 listA = ["EDU_0010_ani01_v001_w01.mov",
          "EDU_0010_mmv01_v003_w02.mov",
@@ -193,5 +198,35 @@ listA = ["EDU_0010_ani01_v001_w01.mov",
 dictA = [{"shot_name": "EDU_0010", "task_name": "ani01", "path": "/projects/2022_09_pipelineEDU2/sequence/EDU/EDU_0010"},
          {"shot_name": "INJ_0010", "task_name": "cmp01", "path": "/projects/2023_06_theKillers/sequence/INJ/INJ_0010"},
          {"shot_name": "TST_0010", "task_name": "lgt01", "path": "/projects/2022_06_lostArk/sequence/TST/TST_0010"}]
+
+{"2023_06_theKillers" : "/projects/2023_06_theKillers"}
+{"hands": "hands_right_Ctrl"}
+
+pathA = "/projects/2022_09_pipelineEDU2/seq"
+
+for i in list(dictA.keys()):
+    if i in pathA:
+        pathB = pathA.replace(i, dictA[i])
+        print(pathB)
+dictA = {"/projects": "X:\\projects", "/usersetup/liuxt": "Z:\\linux"}
+
+
+
+import json
+
+json_file_path = "/projects/..../.json"
+def load_json(self, json_file_path: str):
+    loadJ = open(json_file_path, "r").read()
+    data = json.loads(loadJ)
+    return data
+
+with open(json_file_path, 'w') as f:
+    json.dump(json_data, f, indent=4)
+
+def save_json(self, json_file_path: str, json_data: dict):
+    with open(json_file_path, 'w') as f:
+        json.dump(json_data, f, indent=4)
+
+
 
 
